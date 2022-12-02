2b59d36481acf946e06d22e37088d35c1b2ae2b1
Alex Petitjean <alex.m.petitjean@gmail.com>
2022-12-01 18:09:21 -0600

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map(String::toIntOrNull)
+            .fold(emptyList<List<Int>>()) { acc, i ->
+                if (i == null) {
+                    acc + listOf(emptyList())
+                } else {
+                    acc.dropLast(1) + listOf((acc.lastOrNull() ?: emptyList()) + i)
+                }
+            }
+            .maxOfOrNull { it.sum() }
+            ?: -1
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .asSequence()
+            .map(String::toIntOrNull)
+            .fold(emptyList<List<Int>>()) { acc, i ->
+                if (i == null) {
+                    acc + listOf(emptyList())
+                } else {
+                    acc.dropLast(1) + listOf((acc.lastOrNull() ?: emptyList()) + i)
+                }
+            }
+            .sortedByDescending(List<Int>::sum)
+            .take(3)
+            .sumOf(List<Int>::sum)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24_000)
 
     val input = readInput("Day01")
     println(part1(input))
